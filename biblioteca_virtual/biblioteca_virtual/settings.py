d2c9efdc6b2158dbd31c8a8d2e7a1e4bdf93ae1e
Santu <gianni.d.santucci@gmail.com>
2016-01-29 13:55:41 -0430

    Update settings.py

--- biblioteca_virtual/biblioteca_virtual/settings.py
+++ biblioteca_virtual/biblioteca_virtual/settings.py
@@ -25,7 +25,7 @@ DATABASES = {
         'NAME': 'biblioteca_virtual',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': 'machurucuto666',
+        'PASSWORD': 'xxxx123456cc',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432',                     # Set to empty string for default.
     }
